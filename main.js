c82caad7e3a0576bdb4fc3dd986d4eda6fcf130e
XD2085 <gs0976906273@gmail.com>
2025-09-21 14:57:01 +0800

    asdads

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-import "./assets/scss/all.scss";
-import "bootstrap/dist/js/bootstrap.min.js";
+// 匯入 Bootstrap 的 CSS 檔案
+import "bootstrap/dist/css/bootstrap.min.css";
 
-console.log("Hello world");
+// 匯入 Bootstrap 的 JavaScript 檔案
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// 匯入你自訂的 SCSS 入口檔案
+import "./assets/scss/all.scss";
