43f3f896851277f39ef155798f538d33229e7219
BeeLu0225 <bee810225@hotmail.com>
2025-10-26 16:05:43 +0800

    cherry-pick Q&A-Page分支
    # Conflicts:
    #	assets/JS/Q&A-Page.js

--- assets/JS/Q&A-Page.js
+++ assets/JS/Q&A-Page.js
@@ -1,5 +1,16 @@
 // 導入 Bootstrap 的 JavaScript
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// // 導入這個頁面專屬的 SCSS 檔案
-// import "../scss/_custom.scss";
+// 導入這個頁面專屬的 SCSS 檔案
+import "../scss/_custom.scss";
+
+const siteApp = document.querySelector('[data-app="site"]');
+
+if (siteApp) {
+  console.log("這段程式只在 site 區域執行");
+
+  const btn = siteApp.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    alert("這是 site 區域的按鈕！");
+  });
+}
