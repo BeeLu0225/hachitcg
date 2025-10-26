508fa3744289ee3660b2eaa37ce6a2481c45831b
BeeLu0225 <bee810225@hotmail.com>
2025-10-26 16:07:32 +0800

    移除QAPage.scss直接讀取custom & 新增Link

--- assets/JS/Q&A-Page.js
+++ assets/JS/Q&A-Page.js
@@ -1,9 +1,6 @@
 // 導入 Bootstrap 的 JavaScript
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// 導入這個頁面專屬的 SCSS 檔案
-import "../scss/_custom.scss";
-
 const siteApp = document.querySelector('[data-app="site"]');
 
 if (siteApp) {
