9e04aa85bc46697f2081cfcc085a21ba4e3ae97d
BeeLu0225 <bee810225@hotmail.com>
2025-10-26 15:08:23 +0800

    修正Q&A-Page 直接讀custom.scss的BUG

--- assets/JS/Q&A-Page.js
+++ assets/JS/Q&A-Page.js
@@ -1,5 +1,5 @@
 // 導入 Bootstrap 的 JavaScript
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// 導入這個頁面專屬的 SCSS 檔案
-import "../scss/_custom.scss";
+// // 導入這個頁面專屬的 SCSS 檔案
+// import "../scss/_custom.scss";
